b0d62433ca68f3bbefa2e4ebd4c7cf7d68434805
JamesORourke <geekjamesorourkejr@icloud.com>
2017-07-24 13:06:09 -0700

    Assignement 1 for JavaScript module.

---
+++ IDoDeclare.js
@@ -0,0 +1,26 @@
+//numbers
+var count = 0;
+var inchPerFoot = 12;
+var Senators = 100;
+var BradyFamilyWithAlice = 9;
+
+//Strings
+var Name = "James ORourke";
+var CanIUseSingleQuotes = 'Yes you can';
+var AssignmentTitle = "I Do Declare!";
+
+//Booleans
+var isThisCoding = true;
+var isThisPottery = false;
+
+var UndefinedVariable = null;
+
+console.log("The first  String: " , Name);
+console.log("The second String: " , CanIUseSingleQuotes);
+console.log("The third  String: " , AssignmentTitle);
+console.log("The first  number: ", count)
+console.log("The second number: ", inchPerFoot);
+console.log("The third  number: ", Senators);
+console.log("The fourth number: ", BradyFamilyWithAlice);
+console.log("The first  boolean: ", isThisCoding);
+console.log("The Second boolean: ", isThisPottery);
